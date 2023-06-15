457995def2ed9dde2ce4e8287faff16cd5906907
Mirado <hei.mirado@gmail.com>
2023-06-15 10:36:10 +0300

    add different time

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import moment from "moment";
 
 /**
   Calculates the time difference between the server time and client time.
@@ -7,11 +9,33 @@ import { useRouter } from "next/router";
   @param {Date} clientTime - The client time.
   @returns {string} The time difference in the format "{days} days, {hours} hours, {minutes} minutes, {seconds} seconds".
 */
-const calculateTimeDifference = (server: Date, client: Date) => {};
 
+const calculateTimeDifference = (serverTime: Date, clientTime: Date) => {
+  const difference = moment(serverTime).diff(clientTime);
+  const duration = moment.duration(difference);
+  const days = Math.floor(duration.asDays());
+  const hours = duration.hours();
+  const minutes = duration.minutes();
+  const seconds = duration.seconds();
+  return `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+  
+};
+export async function getServerSideProps() {
+  const serverTime = new Date().toISOString();
+  return { props: { serverTime } };
+}
 
-export default function Home() {
+export default function Home({serverTime}) {
   const router = useRouter();
+
+  const [timeDifference, setTimeDifference] = useState("");
+
+  useEffect(() => {
+    const clientTime = new Date();
+    const difference = calculateTimeDifference(serverTime, clientTime);
+    setTimeDifference(difference);
+  }, [serverTime]);
+  
   const moveToTaskManager = () => {
     router.push("/tasks");
   }
@@ -29,13 +53,13 @@ export default function Home() {
           {/* Display here the server time (DD-MM-AAAA HH:mm)*/}
           <p>
             Server time:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
+            <span className="serverTime">{moment(serverTime).format("DD-MM-YYYY HH:mm")}</span>
           </p>
 
           {/* Display here the time difference between the server side and the client side */}
           <p>
             Time diff:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
+            <span className="serverTime">{timeDifference}</span>
           </p>
         </div>
 
@@ -46,3 +70,4 @@ export default function Home() {
     </>
   );
 }
+
