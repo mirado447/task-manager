379e6199368b0559da29ed64edf7e6ba16d0b73e
Mirado <hei.mirado@gmail.com>
2023-06-15 11:14:32 +0300

    add function to useTaskManager

--- src/store/useTaskManager.ts
+++ src/store/useTaskManager.ts
@@ -1,7 +1,35 @@
-const useTaskManager = () => {
+import create, { SetState } from "zustand";
 
-}
+type Task = {
+  id: number;
+  title: string;
+};
 
-export {
-  useTaskManager
-}
+type TaskManagerState = {
+  tasks: Task[];
+  searchQuery: string;
+  setSearchQuery: (query: string) => void;
+  addTask: (task: Task) => void;
+  updateTask: (taskId: number, updatedTask: Partial<Task>) => void;
+  deleteTask: (taskId: number) => void;
+};
+
+const useTaskManager = create<TaskManagerState>((set: SetState<TaskManagerState>) => ({
+  tasks: [],
+  searchQuery: "",
+  setSearchQuery: (query: string) => set(() => ({ searchQuery: query })),
+  addTask: (task: Task) =>
+    set((state: TaskManagerState) => ({ tasks: [...state.tasks, task] })),
+  updateTask: (taskId: number, updatedTask: Partial<Task>) =>
+    set((state: TaskManagerState) => ({
+      tasks: state.tasks.map((task) =>
+        task.id === taskId ? { ...task, ...updatedTask } : task
+      ),
+    })),
+  deleteTask: (taskId: number) =>
+    set((state: TaskManagerState) => ({
+      tasks: state.tasks.filter((task) => task.id !== taskId),
+    })),
+}));
+
+export default useTaskManager;
